262764a7981caca0bf1ea20fd3ed6c268ce1eab8
FredCT <freddycarrillotucto@hotmail.com>
2020-09-09 10:05:23 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicación de  comandos
+
+Ejercicio del curso NodeJS 
+
+Recuerden instalar los paquetes de node
+
+```
+ npm install
+```
---
+++ config/yargs.js
@@ -0,0 +1,32 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+};
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como compleado o pendiente la tarea'
+};
+
+
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('listar', 'Listar todas las tareas pendientes')
+    .command('borrar', 'Borrar una tarea', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
